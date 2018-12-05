96b834104e697df85b0ef50884deacf55c8017c2
山本達也 <>
2018-12-05 17:38:30 +0900

    fizzの作成

---
+++ fizzBuzz.c
@@ -0,0 +1,16 @@
+//
+//  fizz.c
+//  
+//
+//  Created by 山本達也 on 2018/11/29.
+//
+
+#include "fizzBuzz.h"
+
+int main(void){
+    for(int = 0; i < 100; i++){
+        if(i % 3 == 0){
+            printf("Fizz\n");
+        }
+    }
+}
---
+++ fizzBuzz.h
@@ -0,0 +1,13 @@
+//
+//  fizz.h
+//  
+//
+//  Created by 山本達也 on 2018/11/29.
+//
+
+#ifndef fizzBuzz_h
+#define fizzBuzz_h
+
+#include <stdio.h>
+
+#endif /* fizzBuzz_h */
